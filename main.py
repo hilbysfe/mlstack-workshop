657a93bf591941c08ac59c54e095e49866d60d37
AdamHilbert <hilbysfe@gmail.com>
2021-11-09 11:46:57 +0100

    [!!] fixed history retrieve [++] added predict logic

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import Optional
 from fastapi import FastAPI
 import yfinance
 import datetime
+import numpy as np
 
 app = FastAPI()
 
@@ -9,13 +10,16 @@ def fetch_ticker(symbol: str):
     return yfinance.Ticker(symbol)
 
 def calc_prediction(ticker, days: int):
-    return ticker.history(
+    # Get history
+    data = ticker.history(
                     start=(datetime.date.today() - datetime.timedelta(days=days)).isoformat(),
                     end=datetime.date.today().isoformat()
-                    )
+                    )["Open"].to_numpy()
+    # Calculate prediction
+    return np.mean(data)
 
 @app.get("/prediction/")
-async def root(ticker: str, history: int = 1):
+async def root(ticker: str, history: int = 10):
     
     # Call yahoo API for history
     y_ticker = fetch_ticker(ticker)
